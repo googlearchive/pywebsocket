153d95c456bc9fcc8c664c1056d52aff9ad381ba
tyoshino <tyoshino@chromium.org>
2011-07-05 02:53:07 +0000

    Change exceptions to inherit Exception not RuntimeError.

--- src/mod_pywebsocket/_stream_base.py
+++ src/mod_pywebsocket/_stream_base.py
@@ -43,10 +43,13 @@ from mod_pywebsocket import util
 
 
 # Exceptions
+
+
 class ConnectionTerminatedException(Exception):
     """This exception will be raised when a connection is terminated
     unexpectedly.
     """
+
     pass
 
 
@@ -54,22 +57,25 @@ class InvalidFrameException(ConnectionTerminatedException):
     """This exception will be raised when we received an invalid frame we
     cannot parse.
     """
+
     pass
 
 
-class BadOperationException(RuntimeError):
+class BadOperationException(Exception):
     """This exception will be raised when send_message() is called on
     server-terminated connection or receive_message() is called on
     client-terminated connection.
     """
+
     pass
 
 
-class UnsupportedFrameException(RuntimeError):
+class UnsupportedFrameException(Exception):
     """This exception will be raised when we receive a frame with flag, opcode
     we cannot handle. Handlers can just catch and ignore this exception and
     call receive_message() again to continue processing the next frame.
     """
+
     pass
 
 

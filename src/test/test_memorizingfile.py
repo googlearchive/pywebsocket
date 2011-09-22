55ce0564e575108ca590fbfd4bf6c74bdb430ef7
toyoshim <toyoshim@chromium.org>
2011-09-22 08:19:45 +0000

    Support Python 2.7.
    BUG=http://code.google.com/p/pywebsocket/issues/detail?id=101
    
    Review URL: http://codereview.appspot.com/4968073

--- src/test/test_memorizingfile.py
+++ src/test/test_memorizingfile.py
@@ -52,6 +52,29 @@ class UtilTest(unittest.TestCase):
         for expected, actual in zip(expected_list, actual_list):
             self.assertEqual(expected, actual)
 
+    def check_with_size(self, memorizing_file, read_size, expected_list):
+        read_list = []
+        read_line = ''
+        while True:
+            line = memorizing_file.readline(read_size)
+            line_length = len(line)
+            self.assertTrue(line_length <= read_size)
+            if line_length == 0:
+                if read_line != '':
+                    read_list.append(read_line)
+                break
+            read_line += line
+            if line[line_length - 1] == '\n':
+                read_list.append(read_line)
+                read_line = ''
+        actual_list = memorizing_file.get_memorized_lines()
+        self.assertEqual(len(expected_list), len(actual_list))
+        self.assertEqual(len(expected_list), len(read_list))
+        for expected, actual, read in zip(expected_list, actual_list,
+                                          read_list):
+            self.assertEqual(expected, actual)
+            self.assertEqual(expected, read)
+
     def test_get_memorized_lines(self):
         memorizing_file = memorizingfile.MemorizingFile(StringIO.StringIO(
                 'Hello\nWorld\nWelcome'))
@@ -67,6 +90,12 @@ class UtilTest(unittest.TestCase):
                 ''))
         self.check(memorizing_file, 10, [])
 
+    def test_get_memorized_lines_with_size(self):
+        for size in range(1,10):
+            memorizing_file = memorizingfile.MemorizingFile(StringIO.StringIO(
+                'Hello\nWorld\nWelcome'))
+            self.check_with_size(memorizing_file, size,
+                                 ['Hello\n', 'World\n', 'Welcome'])
 
 if __name__ == '__main__':
     unittest.main()

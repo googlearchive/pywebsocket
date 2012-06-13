af996077b86039ca3d7a0da117d3d6a3cbbe8064
toyoshim <toyoshim@chromium.org>
2012-06-13 08:51:26 +0000

    Update version from 0.7.5 to 0.7.6
    TBR=tyoshino@chromium.org,bashi@chromium.org
    
    Review URL: https://codereview.appspot.com/6300090

--- src/setup.py
+++ src/setup.py
@@ -57,7 +57,7 @@ setup(author='Yuzo Fujishima',
       url='http://code.google.com/p/pywebsocket/',
       # See the source of distutils.version, distutils.versionpredicate and
       # distutils.dist to understand how to name version numbers.
-      version='0.7.5',
+      version='0.7.6',
       )
 
 
